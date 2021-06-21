9446da16994914c60123bf48be2edfbc64ccb3eb
Michael-Kochis <rhishisikk@gmail.com>
2021-06-21 12:21:42 -0500

    Added redux-thunk to main index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { applyMiddleware, createStore } from 'redux';
+import { Provider} from 'react-redux';
+import thunk from 'redux-thunk'
+import { primeReducer } from './js/reducers'
+
+const store = createStore(primeReducer, applyMiddleware(thunk) );
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
