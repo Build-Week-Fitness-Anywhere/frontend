3c09653ea44609dbd0cfaae7740910eaa6f54f4f
Michael-Kochis <rhishisikk@gmail.com>
2021-06-24 13:44:13 -0500

    modified axios call

--- src/actions/classActions.js
+++ src/actions/classActions.js
@@ -76,7 +76,9 @@ const editClass = (item) => {
         }
         let newClasses = getState().class;
         
-       axiosWithAuth().put(`/api/classes/:class_id/${item.class_id}`, neoClass)
+        console.log(item.class_id);
+        const destString = "/api/classes/:class_id/" + item.class_id
+        axiosWithAuth().put(destString, neoClass)
             .then((resp) => {
                 newClasses = newClasses.filter((thing) => {
                     return (thing.id !== item.id );
